0a9418f512d796bf4fbe6a85b413c5884dbe0606
avinashbharti97 <avinashbharti97@gmail.com>
2019-01-20 23:54:33 +0530

    material-ui depreciated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,48 +1,16 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-import { sizing } from '@material-ui/system';
-import Typography from '@material-ui/core/Typography';
 
-const styles = theme => ({
-    root: {
-      flexGrow: 1,
-    },
-    paper: {
-      padding: theme.spacing.unit * 2,
-      textAlign: 'center',
-      color: theme.palette.text.secondary,
-    },
-    introDiv: {
-    
-    }
-  });
-  
-  function Home(props) {
-    const { classes } = props;
-  
-    return (
-      <div className={classes.root}>
-        <Grid container spacing={0}>
-          <Grid item xs={12} sm={6}> 
-            <Typography component="h2" variant="display4" gutterBottom>
-            NIT-Kkr
-            </Typography>
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <Paper className={classes.paper}>
-
-            </Paper>
-          </Grid>
-        </Grid>
-      </div>
-    );
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
   }
-  
-  Home.propTypes = {
-    classes: PropTypes.object.isRequired,
-  };
-  
-  export default withStyles(styles)(Home);
+  render() { 
+    return ( 
+      <div>home</div>
+     );
+  }
+}
+ 
+export default Home;
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,58 +2,23 @@ import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 
-
-
-const styles = {
-    root: {
-      flexGrow: 1,
-    },
-    grow: {
-      flexGrow: 1,
-    },
-    menuButton: {
-      marginLeft: -12,
-      marginRight: 20,
-    },
-    link: {
-      margin: 12.5,
-      color:'white',
-      textDecoration: 'none'
-    },
-  };
-
-  function Navigation(props) {
-    const { classes } = props;
-    return (
-
-      <div className={classes.root}>
-        <AppBar position="static" color='secondary'>
-          <Toolbar>
-            <Typography variant="h6" color="inherit" className={classes.grow}>
-              NITKOSG
-            </Typography>
-                <NavLink color="inherit" className={classes.link} to="/">Home</NavLink>
-                <NavLink color="inherit" className={classes.link} to="/projects">Projects</NavLink>
-                <NavLink color="inherit" className={classes.link} to="/events">Events</NavLink>
-                <NavLink color="inherit" className={classes.link} to="/tutorials">Tutorials</NavLink>
-                <NavLink color="inherit" className={classes.link} to="/members">Members</NavLink>
-          </Toolbar>
-        </AppBar>
-      </div>
-    );
-  }
-  
-  Navigation.propTypes = {
-    classes: PropTypes.object.isRequired,
-  };
-  
-  export default withStyles(styles)(Navigation);
-
+class Navigation extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (
+            <div>
+                <NavLink color="inherit" to="/">Home</NavLink>
+                <NavLink color="inherit" to="/projects">Projects</NavLink>
+                <NavLink color="inherit" to="/events">Events</NavLink>
+                <NavLink color="inherit" to="/tutorials">Tutorials</NavLink>
+                <NavLink color="inherit" to="/members">Members</NavLink>
+            </div>
+          );
+    }
+}
+ 
+export default Navigation;
