3a2bbdc7852b02155eb33a41ac0c915b9c4672c6
saranonearth <saran.suresh@outlook.com>
2019-04-19 12:28:16 +0530

    Updates

---
+++ precache-manifest.942ed8b95e815e3126330b585fac4f41.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "53368b299c2437eab70336e469f52b17",
+    "url": "/NITKOSG_Website/static/media/pic2.53368b29.png"
+  },
+  {
+    "revision": "244730a4ae4417efb17b",
+    "url": "/NITKOSG_Website/static/css/main.88fd9572.chunk.css"
+  },
+  {
+    "revision": "fe5aeef0b75ebada8ef9fb9fe7c07c37",
+    "url": "/NITKOSG_Website/static/media/hero.fe5aeef0.jpg"
+  },
+  {
+    "revision": "4ec210ba1112a8d9412e",
+    "url": "/NITKOSG_Website/static/js/1.4ec210ba.chunk.js"
+  },
+  {
+    "revision": "0fedf2a44886bd117a5c",
+    "url": "/NITKOSG_Website/static/js/runtime~main.0fedf2a4.js"
+  },
+  {
+    "revision": "b6e6ddbde5aa10a0eac3ed45cc3a5dd2",
+    "url": "/NITKOSG_Website/static/media/pic1.b6e6ddbd.png"
+  },
+  {
+    "revision": "244730a4ae4417efb17b",
+    "url": "/NITKOSG_Website/static/js/main.244730a4.chunk.js"
+  },
+  {
+    "revision": "8650311e51c0b2aa170773c8d0719f2b",
+    "url": "/NITKOSG_Website/static/media/pic3.8650311e.png"
+  },
+  {
+    "revision": "d5ad159b83b28f23b4e6d35a1c25b0d0",
+    "url": "/NITKOSG_Website/static/media/pic4.d5ad159b.png"
+  },
+  {
+    "revision": "9b3aa1b191da3eddb388836ddd8b8b0f",
+    "url": "/NITKOSG_Website/static/media/SofiaProSemiBold.9b3aa1b1.woff"
+  },
+  {
+    "revision": "4ec210ba1112a8d9412e",
+    "url": "/NITKOSG_Website/static/css/1.5de11829.chunk.css"
+  },
+  {
+    "revision": "a7a4fa9bd7de43d634d67d6ac45e6448",
+    "url": "/NITKOSG_Website/index.html"
+  }
+];
