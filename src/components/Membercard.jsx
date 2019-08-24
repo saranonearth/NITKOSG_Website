ebd58c4dda222d3945ef80654fe4974c0695ebeb
saranonearth <saran.suresh@outlook.com>
2019-08-24 23:38:25 +0530

    Fullpage js removed

--- src/components/Membercard.jsx
+++ src/components/Membercard.jsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import { Card, CardTitle, CardSubtitle, Row, Col } from 'reactstrap';
-
+import { Card, CardTitle, CardSubtitle } from 'reactstrap';
 
 class Membercard extends React.Component {
-	constructor(props) {
-		super(props);
-	}
-	render() {
-
-
-		return(
-					<div>
-					      
-					        <Card body className=" text-center">
-					          <img width="100%"  id="person-img" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=picture&w=300&h=300" alt="Card image cap" />
-					          <CardTitle className="">{this.props.mem.name}</CardTitle>
-					          <CardSubtitle>{this.props.mem.description}</CardSubtitle>
-					        </Card>
-					      
-				</div>
-			);
-	}
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <Card body className=' text-center'>
+          <img
+            width='100%'
+            id='person-img'
+            src='https://placeholdit.imgix.net/~text?txtsize=33&txt=picture&w=300&h=300'
+            alt='Card image cap'
+          />
+          <CardTitle className=''>{this.props.mem.name}</CardTitle>
+          <CardSubtitle>{this.props.mem.description}</CardSubtitle>
+        </Card>
+      </div>
+    );
+  }
 }
 
 export default Membercard;
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,66 +1,105 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import {
-    Collapse,
-    Navbar,
-    NavbarToggler,
-    NavbarBrand,
-    Nav,
-    NavItem,
-    UncontrolledDropdown,
-    DropdownToggle,
-    DropdownMenu,
-    DropdownItem, Button } from 'reactstrap';
-import './Navigation.css'
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  Button
+} from 'reactstrap';
+import './Navigation.css';
 class Navigation extends Component {
-    constructor(props) {
-        super(props);
-        this.toggle = this.toggle.bind(this);
-        this.state = { 
-            isOpen: false
-         };
-    }
-    toggle() {
-        this.setState({
-          isOpen: !this.state.isOpen
-        });
-      }
-    
+  constructor(props) {
+    super(props);
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
 
-
-    render() { 
-    
-
-        return (
-        <div>
-            <Navbar  style={{backgroundColor: '#9872EC'}}light expand="md">
-            <NavbarBrand>
-                <NavLink to="/" className=" text-white" style={{textDecoration:'none'}}>NITKOSG</NavLink>
-            </NavbarBrand>
-            <NavbarToggler onClick={this.toggle} />
-                <Collapse isOpen={this.state.isOpen} navbar>
-                    <Nav className="ml-auto" navbar>
-                        <NavItem>
-                            <NavLink className="m-3 text-white" to="/projects">Projects</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink className="m-3 text-white" to="/events">Events</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink className="m-3  text-white" to="/members">Members</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink className="m-3  text-white" to="/tutorials">Tutorials</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <Button href="https://nitkosg.slack.com" size="sm" style={{padding:'0.1rem 0.5rem',backgroundColor:'#B397FF',borderColor:'#ECE5FF'}} active>Join Slack</Button>
-                        </NavItem>
-                    </Nav>
-                </Collapse>
-            </Navbar>
-        </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Navbar style={{ backgroundColor: '#9872EC' }} light expand='md'>
+          <NavbarBrand>
+            <NavLink
+              to='/'
+              className=' text-white'
+              style={{ textDecoration: 'none' }}
+            >
+              NITKOSG
+            </NavLink>
+          </NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className='ml-auto' navbar>
+              <NavItem>
+                <NavLink
+                  onClick={this.toggle}
+                  className='m-3 text-white'
+                  to='/projects'
+                >
+                  Projects
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink
+                  onClick={this.toggle}
+                  className='m-3 text-white'
+                  to='/events'
+                >
+                  Events
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink
+                  onClick={this.toggle}
+                  className='m-3  text-white'
+                  to='/members'
+                >
+                  Members
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink
+                  onClick={this.toggle}
+                  className='m-3  text-white'
+                  to='/tutorials'
+                >
+                  Tutorials
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <Button
+                  href='https://nitkosg.slack.com'
+                  size='sm'
+                  style={{
+                    padding: '0.1rem 0.5rem',
+                    backgroundColor: '#B397FF',
+                    borderColor: '#ECE5FF'
+                  }}
+                  active
+                >
+                  Join Slack
+                </Button>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
 }
- 
+
 export default Navigation;
